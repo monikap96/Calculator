40b8365d3215607dbd8fc6dc388a448986375346
monikap96 <monikap961@gmail.com>
2021-09-06 23:36:35 +0200

    added messages class

---
+++ lib/Messages.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Messages{
+    private $errors = array();
+    private $number = 0;
+    
+    public function addError($message){
+        $this->errors[] = $message;
+        $this->number++;
+    }
+    
+    public function isEmpty() {
+        return $this->number==0;
+    }
+    
+    public function isError() {
+        return count($this->errors)>0;
+    }
+    
+    public function getErrors() {
+        return $this->errors;
+    }
+}
